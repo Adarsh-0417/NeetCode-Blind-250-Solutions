29c8d996d29ee2a3efee7beddbdb3f1e9806495c
adarsh-0417 <adarshsharma0417@gmail.com>
2026-05-08 01:48:37 +0530

    Update LC1_TWO_SUM.cpp

--- Arrays/LC1_TWO_SUM.cpp
+++ Arrays/LC1_TWO_SUM.cpp
@@ -1,50 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
-
         unordered_map<int, int> m;
-
         for (int i = 0; i < arr.size(); i++) {
-
             int first = arr[i];
             int second = target - first;
-
-            // check if required number already exists
             if (m.find(second) != m.end()) {
 
                 return {m[second], i};
             }
-
-            // store current number with index
             m[first] = i;
         }
-
         return {};
     }
 };
 
 int main() {
-
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
-
     Solution obj;
-
     vector<int> ans = obj.twoSum(nums, target);
-
     cout << "Indices are: ";
-
     for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << " ";
     }
-
     cout << endl;
-
     return 0;
 }
